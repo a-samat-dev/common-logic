6a2469b3479e2c3f34f12f3c457635e300c221cd
a.samat.dev <a.samat.dev@gmail.com>
2023-02-16 17:49:42 +0600

    Initial commit

---
+++ src/main/java/kz/smarthealth/commonlogic/exception/CustomException.java
@@ -0,0 +1,16 @@
+package kz.smarthealth.commonlogic.exception;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+}
---
+++ src/test/java/kz/smarthealth/commonlogic/CommonLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.smarthealth.commonlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
